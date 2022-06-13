dcb96dd58aa6a4f5b546562a6ab019cc3ff45304
Miguel Escobar <mae2136@gmail.com>
2022-06-12 20:21:15 -0500

    Add plugins to webpack config. Add database functions. Add PWA functionality to install.js.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -18,7 +20,36 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // Generates webpack, injects bundle.
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      // CSS loader plguin
+      new MiniCssExtractPlugin(),
+      // Creates manifest.json file.
+      new WebpackPwaManifest({
+        name: 'JATE by Miguel',
+        short_name: 'JATE',
+        description: 'Just another text editor. This time by Miguel.',
+        start_url: '/',
+        publicPath: '/',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        crossorigin: null, 
+        icons: [
+          {
+            src: path.resolve('./src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ]
+      }),
+      // Injects service worker.
+      new InjectManifest({
+        swSrc: './src/sw.js',
+        swDest: 'service-worker.js',
+      }), 
     ],
 
     module: {
@@ -33,6 +64,10 @@ module.exports = () => {
               presets: ['@babel/preset-env'],
             },
           },
+        },
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
         },
       ],
     },
